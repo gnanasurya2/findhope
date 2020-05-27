1ce5111ea0517284fa5d499c079b31293e336f44
gnanasurya <gnanasurya2@gmail.com>
2020-05-27 15:54:53 +0530

    Updates

---
+++ precache-manifest.bc138e2e281ffed479804917e3da64e2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca396e85e62402aeed3a75b3e2ad15eb",
+    "url": "/findhope/index.html"
+  },
+  {
+    "revision": "5ddbf3b11bc6ff33d11e",
+    "url": "/findhope/static/css/main.45279595.chunk.css"
+  },
+  {
+    "revision": "8e3602179880cc343b93",
+    "url": "/findhope/static/js/2.707b8327.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/findhope/static/js/2.707b8327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ddbf3b11bc6ff33d11e",
+    "url": "/findhope/static/js/main.e87e6a3e.chunk.js"
+  },
+  {
+    "revision": "7a6ea221958ad584b345",
+    "url": "/findhope/static/js/runtime-main.1cbb8e34.js"
+  },
+  {
+    "revision": "64a8711f9395e60ada4006943e11eed9",
+    "url": "/findhope/static/media/ApercuLight.64a8711f.otf"
+  },
+  {
+    "revision": "9d1acbedcf40f880a3dfc4b5788dc5ca",
+    "url": "/findhope/static/media/first_circle_illustration.9d1acbed.svg"
+  },
+  {
+    "revision": "5b92220d30135aed708799e381c4c195",
+    "url": "/findhope/static/media/logo.5b92220d.jpeg"
+  },
+  {
+    "revision": "18a638871b76fde73a12b4202e284f30",
+    "url": "/findhope/static/media/second_circle_illustration.18a63887.svg"
+  }
+]);
