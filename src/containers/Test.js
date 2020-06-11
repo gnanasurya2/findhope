0db0c19bcc922abb826cca106c914a13ed606ff3
gnanasurya <gnanasurya2@gmail.com>
2020-06-11 16:50:50 +0530

    addded google sign-in

--- src/containers/Test.js
+++ src/containers/Test.js
@@ -11,7 +11,17 @@ import GoogleLogin from "react-google-login";
 const Test = (props) => {
   const [data, setData] = useState({});
   const [redirect, setRedirect] = useState(false);
-
+  const [backgroundcolor, setBackgroundcolor] = useState([
+    "#4592d1",
+    "#dcf0f7",
+  ]);
+  const [formLink, setFormLink] = useState(
+    "https://docs.google.com/forms/u/5/d/e/1FAIpQLSdaUun4-lKr9hkPmjtLQsbl24SPO3-Gh5OS8_JzokqGH8eERA/formResponse"
+  );
+  const [entryValues, setEntryValues] = useState([
+    "entry.1758988128",
+    "entry.1916594965",
+  ]);
   const { testName } = useParams();
   useEffect(() => {
     window.scrollTo({ top: 0 });
@@ -20,42 +30,72 @@ const Test = (props) => {
       setData(TestData.depression);
     } else if (testName === "stress") {
       setData(TestData.stress);
+      setBackgroundcolor(["#fce9a2", "#807a7d"]);
+      setFormLink(
+        "https://docs.google.com/forms/u/3/d/e/1FAIpQLScFCedzWi5UfDtvloVWQKi59_0t4A0eQV8dQqjD6oyW7xOMYg/formResponse"
+      );
+      setEntryValues(["entry.457685019", "entry.1693599908"]);
     } else if (testName === "anxiety") {
       setData(TestData.anxiety);
+      setBackgroundcolor(["#6992d9", "#d9d9a9"]);
+      setFormLink(
+        "https://docs.google.com/forms/u/0/d/e/1FAIpQLScKqnCynizG-eS7MOHM6OTCd8IKEHACutTmmC_bcwV7j0xiwQ/formResponse"
+      );
+      setEntryValues(["entry.1539871518", "entry.1418110594"]);
     } else {
       setRedirect(true);
     }
   }, [testName]);
   const googleLoginHandler = (response) => {
+    let body = {
+      "entry.1539871518": response.profileObj.name.split(" ").join(""),
+      "entry.1418110594": response.profileObj.email,
+    };
+    console.log(body);
     fetch(
-      "https://docs.google.com/forms/u/5/d/e/1FAIpQLSdaUun4-lKr9hkPmjtLQsbl24SPO3-Gh5OS8_JzokqGH8eERA/formResponse",
+      `https://cors-anywhere.herokuapp.com/${formLink}?${entryValues[0]}=${response.profileObj.name}&${entryValues[1]}=${response.profileObj.email}`,
       {
         method: "POST",
         headers: { Content: "xml" },
-        body: JSON.stringify({
-          "entry.1758988128": "gnanasurya",
-          "entry.1916594965": "gnanasurya2@gmail.com",
-        }),
       }
     )
       .then(() => console.log("success"))
       .catch((err) => console.log(err));
-    console.log(response);
+    console.log(response.profileObj.name);
+    setRedirect(true);
   };
   return (
     <div>
-      {redirect ? <Redirect to="/test" /> : null}
+      {redirect ? (
+        <Redirect
+          to={`/questions/${
+            testName.charAt(0).toUpperCase() + testName.slice(1)
+          }`}
+        />
+      ) : null}
       <div className={styles.wrapper}>
-        <div className={styles.background}>
-          <div className={styles.square1}></div>
-          <div className={styles.square2}></div>
+        <div
+          className={styles.background}
+          style={{ backgroundColor: backgroundcolor[0] }}
+        >
+          <div
+            className={styles.square1}
+            style={{ opacity: 0.2, backgroundColor: backgroundcolor[1] }}
+          ></div>
+          <div
+            className={styles.square2}
+            style={{ opacity: 0.2, backgroundColor: backgroundcolor[1] }}
+          ></div>
         </div>
-        <div className={styles.mainTitleContentContainer}>
+        <div
+          className={styles.mainTitleContentContainer}
+          style={{ color: backgroundcolor[1] }}
+        >
           <h1 className={styles.mainTitle}>{data.title}</h1>
           <p className={styles.mainContent}>{data.titleContent}</p>
         </div>
       </div>
-      <InfoBox background="#4592d1" color="white">
+      <InfoBox background={backgroundcolor[0]} color={backgroundcolor[1]}>
         <h1 className={styles.statTitle}>{data.stat}</h1>
         <p className={styles.statContent}>
           Indian adults suffer from {data.name}
