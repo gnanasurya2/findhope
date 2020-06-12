64e7b448a454de088040576a4c5ec158b091ea24
gnanasurya <gnanasurya2@gmail.com>
2020-06-12 20:39:35 +0530

    Updates

---
+++ precache-manifest.139e67af800efb597cf33c2c469ef2d5.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a0373a3684779231e0664a227d105d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e15f4f5d9c16912830c1",
+    "url": "./static/css/main.25ccf7f2.chunk.css"
+  },
+  {
+    "revision": "5d019bfe3252bca62786",
+    "url": "./static/js/2.c56dcd7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c56dcd7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15f4f5d9c16912830c1",
+    "url": "./static/js/main.c6696d7b.chunk.js"
+  },
+  {
+    "revision": "33691b719a7aa377fe7b",
+    "url": "./static/js/runtime-main.95736a04.js"
+  },
+  {
+    "revision": "1d7914480e456014915ae25bb7f29638",
+    "url": "./static/media/AlternateIllustration.1d791448.svg"
+  },
+  {
+    "revision": "359120e3a71f430f822a7b40f9767894",
+    "url": "./static/media/Apercu Bold.359120e3.otf"
+  },
+  {
+    "revision": "64a8711f9395e60ada4006943e11eed9",
+    "url": "./static/media/ApercuLight.64a8711f.otf"
+  },
+  {
+    "revision": "7c5e1972518d9131f500a5baca1b3956",
+    "url": "./static/media/BackgroundElements.7c5e1972.svg"
+  },
+  {
+    "revision": "30cdec984aaa9e2a3727c89e82ce5d77",
+    "url": "./static/media/FlyingIllustration.30cdec98.svg"
+  },
+  {
+    "revision": "bc64e4d1784f4280658395c58420be3d",
+    "url": "./static/media/HeroPageIllustration.bc64e4d1.svg"
+  },
+  {
+    "revision": "9568d0983602249ac6003e15f09386f7",
+    "url": "./static/media/HomePageLastIllustration.9568d098.svg"
+  },
+  {
+    "revision": "71ca3459c62eaa42b70659a81666eb46",
+    "url": "./static/media/Paraardha Goel.71ca3459.png"
+  },
+  {
+    "revision": "238f9e6d6efcc91adf6dc6e6d00ed27b",
+    "url": "./static/media/Riya aggarwal.238f9e6d.png"
+  },
+  {
+    "revision": "7094eaa511f75c63b03e9d9ce4118426",
+    "url": "./static/media/Sahithi Mannar.7094eaa5.png"
+  },
+  {
+    "revision": "9eeae4258e6faab014b27fe7838e7cd7",
+    "url": "./static/media/Swetha Desiraju.9eeae425.png"
+  },
+  {
+    "revision": "1b539efa85862813d574744dfcc65074",
+    "url": "./static/media/arrow.1b539efa.svg"
+  },
+  {
+    "revision": "0c744c525f1a73dd850d034eec752768",
+    "url": "./static/media/back.0c744c52.svg"
+  },
+  {
+    "revision": "01399f7b287480cd35a31916859f9c84",
+    "url": "./static/media/certified (2).01399f7b.svg"
+  },
+  {
+    "revision": "0a738ca1c047ebe704da221fba779575",
+    "url": "./static/media/certified.0a738ca1.svg"
+  },
+  {
+    "revision": "b76785b3030b0387b6031772022bbf3e",
+    "url": "./static/media/couneslling Hero Illustration.b76785b3.svg"
+  },
+  {
+    "revision": "613a2c8df994fd41f71b0baac049b5cc",
+    "url": "./static/media/counselling Second Illustration.613a2c8d.svg"
+  },
+  {
+    "revision": "0543c2e29172c2690c7686f97c755e08",
+    "url": "./static/media/depression_hero.0543c2e2.svg"
+  },
+  {
+    "revision": "7513c8035297c5c598e38ce0f42ea559",
+    "url": "./static/media/drive the change.7513c803.svg"
+  },
+  {
+    "revision": "1349274cac13cd413729909c2ebbc14a",
+    "url": "./static/media/feelbetter.1349274c.svg"
+  },
+  {
+    "revision": "66678ab421fad42921da2c21f7786f2a",
+    "url": "./static/media/findingsupport.66678ab4.svg"
+  },
+  {
+    "revision": "f83f1779b7e2d05ce37403c87b473c6a",
+    "url": "./static/media/first circle illustration.f83f1779.svg"
+  },
+  {
+    "revision": "9d1acbedcf40f880a3dfc4b5788dc5ca",
+    "url": "./static/media/first_circle_illustration.9d1acbed.svg"
+  },
+  {
+    "revision": "f23dbf67d91e97f0a9c0ade1e27006c3",
+    "url": "./static/media/icons8-checked.f23dbf67.svg"
+  },
+  {
+    "revision": "2399cca2925caf9e17b37f5487794026",
+    "url": "./static/media/icons8-tick-box.2399cca2.svg"
+  },
+  {
+    "revision": "61524034a829af7b09ca9f82a1dd242c",
+    "url": "./static/media/linkedin.61524034.svg"
+  },
+  {
+    "revision": "5b92220d30135aed708799e381c4c195",
+    "url": "./static/media/logo.5b92220d.jpeg"
+  },
+  {
+    "revision": "5f4d2e5accd6c4c65f3de00a8422686e",
+    "url": "./static/media/mail.5f4d2e5a.svg"
+  },
+  {
+    "revision": "218ea7f9af0d1b19ed6aa6ed0258c52f",
+    "url": "./static/media/recognising.218ea7f9.svg"
+  },
+  {
+    "revision": "18a638871b76fde73a12b4202e284f30",
+    "url": "./static/media/second_circle_illustration.18a63887.svg"
+  },
+  {
+    "revision": "d403b1e7001c49a9d75c92e296decc58",
+    "url": "./static/media/stigma.d403b1e7.svg"
+  },
+  {
+    "revision": "74f810febc56809be5d849daaed97c00",
+    "url": "./static/media/time.74f810fe.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/findhope/precache-manifest.2141e93b869e28b77e0c2e150a776c1f.js"
+  "./precache-manifest.139e67af800efb597cf33c2c469ef2d5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/findhope/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
